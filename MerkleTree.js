0164e30eb8a3012706b43077499cbcd50e9a3e55
U~Iriamuzu <daviswill264@gmail.com>
2025-02-02 22:11:44 +0100

    done

--- MerkleTree.js
+++ MerkleTree.js
@@ -9,24 +9,22 @@ const addresses = [
   '0x11223344556677889900aabbccddeeff11223344',
 ];
 
-// console.log('Addresses:', addresses);
-
-const leaves = addresses.map((address) => CryptoJS.SHA256(address).toString());
-// console.log('\nHashed Leaves:', leaves);
-
-const tree = new MerkleTree(leaves, (data) => CryptoJS.SHA256(data).toString());
-
+const leaves = addresses.map((address) => CryptoJS.SHA256(address));
+const tree = new MerkleTree(leaves, (data) => CryptoJS.SHA256(data));
 const root = tree.getRoot().toString('hex');
-// console.log('\nMerkle Root:', root);
-
 const testAddress = '0x1234567890abcdef1234567890abcdef12345678';
-
 const testLeaf = CryptoJS.SHA256(testAddress).toString();
-
 const proof = tree.getProof(testLeaf);
 // console.log('\nProof for Test Address:', proof.map((p) => p.data.toString('hex')));
 
-const isValid = tree.verify(proof, testLeaf, tree.getRoot());
+// Verify the proof
+// Check if the proof matches the root for the test address
+const isValid = tree.verify(proof, testLeaf, root);
 console.log('\nIs the Test Address Valid?', isValid);
+
+const badLeaf = addresses.map(address => CryptoJS.SHA256(address))
+console.log(badLeaf);
+
+
 
 // console.log('\nMerkle Tree Structure:\n', tree.toString());
